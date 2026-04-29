c2fe5d8923b24209bb1d306aa9a1a21204fcd069
jihun-uin <ice0131@duksung.ac.kr>
2026-04-29 23:41:36 +0900

    불필요한 코드 주석 제거 및 플레이어 이동 로직 개선

--- sketch.js
+++ sketch.js
@@ -5,57 +5,6 @@ let y = 350;
 let wx = 200, wy = 150, ww = 50, wh = 100;
 let map;
 
-/*
-
-let dx = [100, 200, 300, 400, 500]; 
-let dy = [150, 250, 100, 300, 200]; 
-let dSize = 15;              
-let dActive = [true, true, true, true, true]; 
-
-let score = 0;
-
-
-function setup() {
-  createCanvas(600, 400);
-  px = width / 2;
-  py = height / 2;
-}
-
-function draw() {
-  background(30);
-  stroke(255,0,0);
-  strokeWeight(4);
-  line(wallX,0,wallX,height);
-  fill(255, 255, 255);
-  textSize(20);
-  text("점수: " + score, 20, 20);
-
-  if (keyIsDown(LEFT_ARROW)) px -= 3;
-  if (keyIsDown(RIGHT_ARROW)) px += 3;
-  if (keyIsDown(UP_ARROW)) py -= 3;
-  if (keyIsDown(DOWN_ARROW)) py += 3;
-
-  fill(255, 255, 0);
-  ellipse(px, py, pd); 
-  
-  for (let i = 0; i < 5; i++) {
-    if (dActive[i] === true) {
-      fill(255, 100, 100);
-      ellipse(dx[i], dy[i], dSize);
-
-      let distance = dist(px, py, dx[i], dy[i]);
-
-      
-      if (distance < (pd / 2) + (dSize / 2)) {
-        dActive[i] = false;
-        score = score + 1;
-      }
-    }
-  }
-}
-*/
-
-
 function movePlayer(nx, ny) {
  if (!(nx > wx && nx < wx + ww && ny > wy && ny < wy + wh))
   {
@@ -74,42 +23,46 @@ function setup(){
 }
 
 function canMove(nx, ny) {
-  if (nx < 0 || nx > width || ny < 0 || ny > height) {
+  if(ny>320 && ny<380){
+    return true;
+  }
+
+  if(nx<0||nx>1000||ny<0||ny>700)
+  {
     return false;
   }
-  if(ny == 350 && nx<0)
-  {
-    movePlayer(1000,350);
-  }
+  
+  return true;
 }
 
-function draw(){
-  background(map);
-  color(2);
-  stroke(255,0,0);
-  strokeWeight(4);
-
-
+function movePacman()
+{
+  let nx = x;
+  let ny = y;
   if (keyIsDown(RIGHT_ARROW)) {
-    if (canMove(x + 5, y)) {
+    if(canMove(x + 5, y))
+    {
       noStroke();
       fill(255, 255, 0);
       ellipse(x, y, 50, 50);
       x += 5;
     }
+
   }
 
   if (keyIsDown(LEFT_ARROW)) {
-     if (canMove(x - 5, y)) {
+    if(canMove(x - 5, y))
+    {
       noStroke();
       fill(255, 255, 0);
       ellipse(x, y, 50, 50);
       x -= 5;
     }
   }
-
+  
   if (keyIsDown(UP_ARROW)) {
-     if (canMove(x, y - 5)) {
+    if(canMove(x, y - 5))
+    {
       noStroke();
       fill(255, 255, 0);
       ellipse(x, y, 50, 50);
@@ -118,17 +71,35 @@ function draw(){
   }
 
   if (keyIsDown(DOWN_ARROW)) {
-     if (canMove(x, y + 5)) {
+    if(canMove(x, y + 5))
+    {
       noStroke();
       fill(255, 255, 0);
       ellipse(x, y, 50, 50);
       y += 5;
     }
   }
+
+  if (ny>320 && ny<380 && nx <-20)
+  {
+    x = 1000;
+    y = ny;
+    return;
+  }
   
+  if (ny>320 && ny<380 && nx >1050)
+  {
+    x = 0;
+    y = ny;
+    return;
+  }
+
   noStroke();
   fill(255, 255, 0);
   arc(x,y,45,45,PI/4,1.7*PI);
+}
 
-
+function draw(){
+  background(map);
+  movePacman();
 }
